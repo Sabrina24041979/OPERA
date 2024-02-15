7551c6c6291851457ab4cf8259df136e472f374d
Sabrina24041979 <sabrina.montassar@yahoo.fr>
2024-02-15 10:15:56 +0100

    Created components

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,13 +1,13 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
 
+// Déclaration de la variable app
 const app = startStimulusApp();
-import { startStimulusApp } from '@symfony/stimulus-bridge';
 
-// Registers Stimulus controllers from controllers.json and in the controllers/ directory
-export const app = startStimulusApp(require.context(
+// Charger les contrôleurs dynamiquement à partir de controllers.json et du dossier controllers/
+export const lazyApp = startStimulusApp(require.context(
     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
     true,
     /\.[jt]sx?$/
 ));
-// register any custom, 3rd party controllers here
+// Enregistrez des contrôleurs personnalisés tiers ici
 // app.register('some_controller_name', SomeImportedController);
